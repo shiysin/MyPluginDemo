5f152b4bb38f30dbf6382ea477019b44ab612710
GitHub Action <action@github.com>
2022-06-19 10:56:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/shiysin/myplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shiysin/myplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shiysin.myplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shiysin.myplugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/shiysin/myplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shiysin/myplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shiysin.myplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shiysin.myplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
